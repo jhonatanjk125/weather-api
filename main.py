e883dd3d1a0e4298bed3baef73e5a9595ddd2eab
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-09-14 02:19:13 -0500

    Add URL endpoint for yearly query and historical data.

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
     dataframe = pd.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20, parse_dates=["    DATE"])
     dataframe['TG0'] = dataframe['   TG'].mask(dataframe['   TG'] == -9999, np.nan)
     dataframe['TG'] = dataframe['TG0'] / 10
@@ -21,6 +20,21 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    dataframe = pd.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20, parse_dates=["    DATE"])
+    result = dataframe.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<date>")
+def yearly(station, date):
+    dataframe = pd.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20)
+    dataframe["    DATE"] = dataframe["    DATE"].astype(str)
+    result = dataframe[dataframe["    DATE"].str.startswith(str(date))].to_dict(orient="records")
+    return result
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
